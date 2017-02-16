fe2a247b4464557d5fe3428549c9e575dd0376a0
lgants <lgants@gmail.com>
2017-02-16 13:27:56 -0600

    completed lesson 7

--- lessons/01-setting-up/modules/Repos.js
+++ lessons/01-setting-up/modules/Repos.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router'
+import NavLink from './NavLink'
 
+// {this.props.children} will render `Repo.js` when at /repos/:userName/:repoName when nested in index.js
 export default React.createClass({
   render() {
     return (
       <div>
         <h2>Repos</h2>
         <ul>
-          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
-          <li><Link to="/repos/facebook/react">React</Link></li>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
         </ul>
+        {this.props.children}
       </div>
     )
   }
