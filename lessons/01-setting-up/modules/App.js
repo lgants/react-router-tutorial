57050887669f56b3e348685b25dfa32f1b2580ae
lgants <lgants@gmail.com>
2017-02-16 11:35:17 -0600

    completed lesson 3

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default React.createClass({
-  render() {
-    return <div>Hello, React Router!</div>
+  render(){
+    <div>
+      <h1>React Router Tutorial</h1>
+      <ul role="nav">
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/repos">Repos</Link></li>
+      </ul>
+    </div>
   }
 })
